7435aac911c95a299b43d4aeaf58e7576613cecf
Jack Roberts <jroberts@turing.ac.uk>
2020-07-10 16:22:00 +0100

    add basic checks for metadata, database and metrics values and formats

--- modmon/db/connect.py
+++ modmon/db/connect.py
@@ -1,4 +1,5 @@
 # import pyodbc
+import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, inspect
 
@@ -15,6 +16,15 @@ DB_CONNECTION_STRING = f"postgresql://localhost:{PORT}/{DB}"
 
 ENGINE = create_engine(DB_CONNECTION_STRING)
 Base.metadata.bind = ENGINE
+
+
+def check_connection_ok():
+    try:
+        with ENGINE.connect() as _:
+            pass
+        return (True, None)
+    except sqlalchemy.exc.OperationalError as e:
+        return (False, e)
 
 
 def get_session():
--- modmon/models/check.py
+++ modmon/models/check.py
@@ -5,10 +5,16 @@ import re
 
 import colorama
 from colorama import Fore
+import dateparser
+import pandas as pd
+from pandas.api.types import is_numeric_dtype
 
+from ..db.connect import get_session, check_connection_ok
+from ..db.schema import Team, Model, Modelversion, Researchquestion, Dataset
 from ..envs.utils import get_model_env_types
 from ..envs.conda import create_conda_env
 from ..envs.renv import create_renv_env
+from ..models.run import build_run_cmd
 
 # reset print colour to default after each use of colorama
 colorama.init(autoreset=True)
@@ -35,44 +41,179 @@ def check_metadata_keys(metadata):
         "team",
         "contact",
         "contact_email",
-        "team_description",
         "research_question",
         "model_name",
-        "model_description",
         "model_version",
         "db_name",
         "data_window_start",
         "data_window_end",
         "model_train_datetime",
-        "training_data_description",
         "model_run_datetime",
-        "test_data_description",
         "command",
     ]
+    optional_keys = [
+        "team_description",
+        "model_description",
+        "training_data_description",
+        "test_data_description",
+    ]
 
-    keys_present = [key in metadata.keys() for key in expected_keys]
-    if all(keys_present):
+    exp_keys_present = [key in metadata.keys() for key in expected_keys]
+    opt_keys_present = [key in metadata.keys() for key in optional_keys]
+    if all(exp_keys_present) and all(opt_keys_present):
         print_success("Metadata: All keys present")
     else:
-        missing_keys = [key for key in expected_keys if key not in metadata.keys()]
-        print_fail(f"Metadata: Missing keys - {missing_keys}")
+        if not all(exp_keys_present):
+            missing_keys = [key for key in expected_keys if key not in metadata.keys()]
+            print_fail(f"Metadata: Missing required keys - {missing_keys}")
+        if not all(opt_keys_present):
+            missing_keys = [key for key in optional_keys if key not in metadata.keys()]
+            print_warn(f"Metadata: Missing optional keys - {missing_keys}")
 
 
 def check_metadata_values(metadata):
-    # valid value and already exists in db or not?
-    print_warn("NOT IMPLEMENTED: check_metadata_values")
-
+    # Check fields which should have specific formats
     checked_values = {}
-    checked_values["contact_email"] = re.fullmatch(
-        r"^[^@\s]+@[^@\s]+\.[^@\s]+$", metadata["contact_email"]
-    )
+
+    # email address
+    if "contact_email" in metadata.keys():
+        checked_values["contact_email"] = (
+            True
+            if re.fullmatch(
+                r"^[^@\s]+@[^@\s]+\.[^@\s]+$", metadata["contact_email"].strip()
+            )
+            else False
+        )
+
+    # datetimes
+    datetime_values = [
+        "data_window_start",
+        "data_window_end",
+        "model_train_datetime",
+        "model_run_datetime",
+    ]
+    for key in datetime_values:
+        if key in metadata.keys():
+            dt = dateparser.parse(metadata[key])
+            if dt:
+                checked_values[key] = True
+            else:
+                checked_values[key] = False
+
+    # no specific format but must not be empty
+    not_empty_values = [
+        "team",
+        "contact",
+        "research_question",
+        "model_name",
+        "model_version",
+        "db_name",
+    ]
+    for key in not_empty_values:
+        if key in metadata.keys():
+            if isinstance(metadata[key], str) and len(metadata[key].strip()) == 0:
+                checked_values[key] = False
+            else:
+                checked_values[key] = True
+
+    # command string - check for placeholders
+    if "command" in metadata.keys():
+        try:
+            build_run_cmd(metadata["command"], "2000-1-1", "2000-1-1", "TEST")
+            checked_values["command"] = True
+        except ValueError:
+            checked_values["command"] = False
+
+    if all(checked_values.values()):
+        print_success("Metadata: All keys have valid values")
+    else:
+        failed_checks = [key for key, check in checked_values.items() if check is False]
+        print_fail(f"Metadata: Keys with invalid values - {failed_checks}")
 
 
-def check_model(model_path):
-    print(f"Checking {model_path}:")
+def check_db_for_duplicates(metadata):
+    ok, err = check_connection_ok()
+    if not ok:
+        print_fail(f"Database: Connection failed - {err}")
+        return
+
+    session = get_session()
+    new_values = []
+    dup_values = []
+
+    if "team" in metadata.keys():
+        if session.query(Team).filter_by(teamname=metadata["team"]).count() == 0:
+            new_values.append("team")
+        else:
+            dup_values.append("team")
+
+    if "research_question" in metadata.keys():
+        if (
+            session.query(Researchquestion)
+            .filter_by(description=metadata["research_question"])
+            .count()
+            == 0
+        ):
+            new_values.append("research_question")
+        else:
+            dup_values.append("research_question")
+
+    if "db_name" in metadata.keys():
+        if (
+            session.query(Dataset).filter_by(databasename=metadata["db_name"]).count()
+            == 0
+        ):
+            new_values.append("db_name")
+        else:
+            dup_values.append("db_name")
+
+    if "model_name" in metadata.keys():
+        model = session.query(Model).filter_by(name=metadata["model_name"]).first()
+        if not model:
+            new_values.append("model_name")
+        else:
+            dup_values.append("model_name")
+
+            if "model_version" in metadata.keys():
+                if (
+                    session.query(Modelversion)
+                    .filter_by(model=model)
+                    .filter_by(modelversion=metadata["model_version"])
+                    .count()
+                    == 0
+                ):
+                    new_values.append("model_version")
+                    print_success("Database: Unique model name and version combination")
+                else:
+                    dup_values.append("model_version")
+                    print_fail("Database: Model name and version already exist")
+
+    if new_values:
+        print_info(f"Database: New entries will be created for {new_values}")
+    if dup_values:
+        print_warn(f"Database: Entries already exist for {dup_values}")
+
+
+def check_metrics_file(metrics_path):
+    metrics = pd.read_csv(metrics_path)
+    metrics.columns = metrics.columns.str.strip()
+
+    exp_cols = ["metric", "value"]
+    if all(metrics.columns == exp_cols):
+        print_success("Metrics: Found expected columns")
+    else:
+        print_fail(
+            f"Metrics: Incorrect columns - found {metrics.columns} instead of {exp_cols}"
+        )
+
+    print_warn("NOT IMPLEMENTED: Numeric value check")
+
+
+def check_submission(path):
+    print(f"Checking {path}:")
 
     # metadata file
-    metadata_path = f"{model_path}/metadata.json"
+    metadata_path = f"{path}/metadata.json"
     if os.path.exists(metadata_path):
         print_success("Metadata: File exists")
 
@@ -82,14 +223,23 @@ def check_model(model_path):
 
             check_metadata_keys(metadata)
             check_metadata_values(metadata)
+            check_db_for_duplicates(metadata)
+
         except json.decoder.JSONDecodeError as e:
             print_fail(f"Metadata: JSON Error - {e}")
 
     else:
         print_fail("Metadata: File not found")
 
+    metrics_path = f"{path}/metrics.csv"
+    if os.path.exists(metrics_path):
+        print_success("Metrics: File exists")
+        check_metrics_file(metrics_path)
+    else:
+        print_fail("Metrics: File not found")
+
     # conda or renv environment
-    env_types = get_model_env_types(model_path)
+    env_types = get_model_env_types(path)
 
     if env_types["conda"]:
         print_success("Environment: conda found")
@@ -98,7 +248,7 @@ def check_model(model_path):
             print_info("Environment: Creating conda env...")
             create_conda_env(
                 "ModMon-TMPCHECK",
-                env_file=f"{model_path}/environment.yml",
+                env_file=f"{path}/environment.yml",
                 capture_output=True,
                 overwrite=True,
             )
@@ -113,7 +263,7 @@ def check_model(model_path):
 
         try:
             print_info("Environment: Creating renv env...")
-            create_renv_env(model_path, capture_output=True)
+            create_renv_env(path, capture_output=True)
             print_success("Environment: renv environment created")
         except subprocess.CalledProcessError as e:
             print_fail(f"Environment: renv creation failed - {e.returncode} {e.stderr}")
