2edfce962d27c960d0c7d650cda0964778cdd167
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-07-20 11:05:17 +0100

    create check_reproduciblity func

--- modmon/models/repro.py
+++ modmon/models/repro.py
@@ -2,11 +2,11 @@ import argparse
 import tempfile
 import shutil # copy
 import subprocess
-# from .run import run_model
+# from ..models.run import run_model
 import pandas as pd
 
-# Temporarily get path from args, but then sctually just use function within check
-# Get start dates and end dates etc from metadat or db
+# Temporarily get path from args, but then actually just use function within check
+# Get start dates and end dates etc from metadata or db
 # Load JSON files from catalogue_results dir within temp dir (only should be 2) and compare the metrics csv hashes
 
 parser = argparse.ArgumentParser(
@@ -17,28 +17,27 @@ parser.add_argument("path", help="Directory to check")
 args = parser.parse_args()
 path = args.path
 
-with tempfile.TemporaryDirectory() as tmpdirname:
-    subprocess.run(["git", "init"], check=True, cwd=tmpdirname)
+def check_reproduciblity(path):
 
-    subprocess.run(["mkdir", tmpdirname + "/data"], check=True)
-    subprocess.run(["mkdir", tmpdirname + "/code"], check=True)
-    subprocess.run(["mkdir", tmpdirname + "/results"], check=True)
-    shutil.copyfile(path + "/metrics.csv", tmpdirname + "/results/metrics.csv")
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        subprocess.run(["git", "init"], check=True, cwd=tmpdirname)
 
-    subprocess.run(["git", "add", "metrics.csv"], check=True, cwd=tmpdirname + "/results")  # check=True <- creates python exception
-    subprocess.run(["git", "commit", "-m", "'add reference result'"], check=True, cwd=tmpdirname)
+        subprocess.run(["mkdir", tmpdirname + "/data"], check=True)
+        subprocess.run(["mkdir", tmpdirname + "/code"], check=True)
+        subprocess.run(["mkdir", tmpdirname + "/results"], check=True)
+        shutil.copyfile(path + "/metrics.csv", tmpdirname + "/results/metrics.csv")
 
-    subprocess.run(["catalogue", "engage",
-                    "--input_data", "data",
-                    "--code", "code"
-                    ], check=True, cwd=tmpdirname)
-    subprocess.run(["catalogue", "disengage",
-                    "--input_data", "data",
-                    "--code", "code",
-                    "--output_data", "results"
-                    ], check=True, cwd=tmpdirname)
+        subprocess.run(["git", "add", "metrics.csv"], check=True, cwd=tmpdirname + "/results")  # check=True <- creates python exception
+        subprocess.run(["git", "commit", "-m", "'add reference result'"], check=True, cwd=tmpdirname)
 
-    test = pd.read_csv(tmpdirname +  "/results/metrics.csv")
-    print(test)
-    # subprocess.run(["cd", tmpdirname], check=True)
-    # subprocess.run(["pwd"], check=True)
+        subprocess.run(["catalogue", "engage",
+                        "--input_data", "data",
+                        "--code", "code"
+                        ], check=True, cwd=tmpdirname)
+        subprocess.run(["catalogue", "disengage",
+                        "--input_data", "data",
+                        "--code", "code",
+                        "--output_data", "results"
+                        ], check=True, cwd=tmpdirname)
+
+check_reproduciblity(path)
